eb38a4759ba08e8eafc55c8fc387112d36cdd4b7
Imran Ariffin <ariffinimran@gmail.com>
2018-04-28 20:23:03 -0400

    \#8: serializer decorator for vote

--- swot_item_vote/serializers.py
+++ swot_item_vote/serializers.py
@@ -1,9 +1,41 @@
-from rest_framework import serializers
+from rest_framework.response import Response
 
-from models import Vote
+from authenticationjwt.models import User
 
 
-class VoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Vote
-        fields = ('id', 'item', 'voteType', 'created_by_id')
+def get_create_username(vote):
+    return User.objects.get(pk=vote.created_by_id).username
+
+
+def serialize(func):
+
+    def serialize_vote(vote):
+        if vote is None:
+            return {}
+        return {
+            'voteId': vote.id,
+            'swotItemId': vote.swot_item_id,
+            'creatorId': vote.created_by_id,
+            'voteType': vote.voteType,
+            'creatorUsername': get_create_username(vote),
+        }
+
+    def _serialize(request, *args, **kwargs):
+        data, status, errors = func(request, *args, **kwargs)
+
+        if errors:
+            return Response({
+                'errors': errors
+            }, status=status)
+
+        if type(data) == list:
+            return Response({
+                'data': [
+                    serialize_vote(vote) for vote in data
+                ]}, status=status)
+
+        return Response({
+            'data': serialize_vote(data)
+        }, status=status)
+
+    return _serialize
--- swot_item_vote/views.py
+++ swot_item_vote/views.py
@@ -1,64 +1,48 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import IntegrityError
 
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
 from swot_item_vote.models import Vote
 from swot_item.models import SwotItem
 
+from .serializers import serialize
+
 from swot.models import Swot
-from authenticationjwt.models import User
 
 from core.decorators import authenticate
 from core.serializers import deserialize
 
 
-def get_create_username(vote):
-    return User.objects.get(pk=vote.created_by_id).username
-
-
-def get_creator_usernames(votes):
-    return dict([
-        (
-            vote.id,
-            get_create_username(vote)
-        ) for vote in votes
-    ])
-
-
 @api_view(['GET'])
 @authenticate
 @deserialize
+@serialize
 def vote_list(request, swot_id):
-    user_id = request.user.id
-    swot = None
 
     try:
-        swot = Swot.objects.get(id=swot_id)
+        Swot.objects.get(id=swot_id)
     except ObjectDoesNotExist:
-        return Response({
-            'errors': ['Swot {} does not exist'.format(swot_id)],
-        }, status=status.HTTP_404_NOT_FOUND)
+        return (
+            None,
+            status.HTTP_404_NOT_FOUND,
+            ['Swot {} does not exist'.format(swot_id)]
+        )
 
     votes = Vote.objects.filter(swot_id=swot_id)
-    creator_usernames = get_creator_usernames(votes)
 
-    return Response({
-        'data': [{
-            'voteId': vote.id,
-            'swotItemId': vote.swot_item_id,
-            'creatorId': vote.created_by_id,
-            'voteType': vote.voteType,
-            'creatorUsername': creator_usernames[vote.id],
-        } for vote in votes]
-    }, status=status.HTTP_200_OK)
+    return (
+        [vote for vote in votes],
+        status.HTTP_200_OK,
+        None
+    )
 
 
 @api_view(['POST'])
 @authenticate
 @deserialize
+@serialize
 def vote(request, swot_item_id):
     swot_item_id = int(swot_item_id)
     vote_type = request.body['voteType']
@@ -68,9 +52,11 @@ def vote(request, swot_item_id):
     try:
         swot_item = SwotItem.objects.get(id=swot_item_id)
     except ObjectDoesNotExist:
-        return Response({
-            'errors': ['Swot Item does {} not exist'.format(swot_item_id)]
-        }, status=status.HTTP_404_NOT_FOUND)
+        return (
+            None,
+            status.HTTP_404_NOT_FOUND,
+            ['Swot Item does {} not exist'.format(swot_item_id)]
+        )
 
     try:
         existing_vote = Vote.objects.get(
@@ -81,7 +67,12 @@ def vote(request, swot_item_id):
         existing_vote.delete()
 
         if existing_vote_type == vote_type:
-            return Response({'data': {}}, status=status.HTTP_200_OK)
+            return (
+                None,
+                status.HTTP_200_OK,
+                None
+            )
+
     except ObjectDoesNotExist:
         pass
 
@@ -94,24 +85,23 @@ def vote(request, swot_item_id):
             voteType=vote_type
         )
     except IntegrityError, ie:
-        return Response(
-            {'errors': [ie]},
-            status=status.HTTP_400_BAD_REQUEST
+        return (
+            None,
+            status.HTTP_400_BAD_REQUEST,
+            [ie]
         )
 
     try:
         vote.save()
     except:
-        return Response(
-            {'errors': ['Error saving vote']},
-            status=status.HTTP_400_BAD_REQUEST)
+        return (
+            None,
+            status.HTTP_400_BAD_REQUEST,
+            ['Error saving vote']
+        )
 
-    return Response({
-        'data': {
-            'voteId': vote.id,
-            'swotItemId': swot_item_id,
-            'creatorId': user_id,
-            'creatorUsername': get_create_username(vote),
-            'voteType': vote_type,
-        }
-    }, status=status.HTTP_201_CREATED)
+    return (
+        vote,
+        status.HTTP_201_CREATED,
+        None,
+    )
